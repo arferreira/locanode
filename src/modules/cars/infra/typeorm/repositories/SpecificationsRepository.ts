7a22a79641b7368dd67194bc6cfbc8d3eefb3b32
Antonio Ricardo Ferreira de Souza <arfs.antonio@gmail.com>
2022-08-12 09:36:19 -0400

    Adjust imports on modules

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,6 +1,5 @@
+import { ISpecificationsRepository } from "../../../../../modules/cars/repositories/ISpecificationsRepository";
 import { getRepository, Repository } from "typeorm";
-import { Specification } from "../../entities/Specification";
-import { ISpecificationsRepository, ICreateSpecificationDTO } from "../ISpecificationsRepository";
 
 class SpecificationRepository implements ISpecificationsRepository {
 
